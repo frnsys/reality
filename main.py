e8459a56d1a5eb6a72fafc545a7a993f03033b44
Francis Tseng <f+accounts@frnsys.com>
2017-05-21 13:06:59 -0400

    logging and fifo listener broadcasting

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import os
+import json
 import logging
+import fasteners
 from time import sleep
 from raven import Client
-from reality import collect
+from reality import collect, hash
+
+
+def broadcast(article):
+    """add an article's data to each listener's FIFO queue"""
+    try:
+        listeners = [l.strip() for l in open('listeners.txt', 'r').readlines()]
+    except FileNotFoundError:
+        listeners = []
+    for path in listeners:
+        with fasteners.InterProcessLock('/tmp/{}.lock'.format(hash(path))):
+            with open(path, 'a') as f:
+                f.write(json.dumps(article)+'\n')
+
 
 if __name__ == '__main__':
     INTERVAL = 60 * 60
     logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    try:
+        dsn = open(os.path.expanduser('~/.sentry_dsn'), 'r').read().strip()
+        client = Client(dsn)
+    except FileNotFoundError:
+        client = None
     while True:
         try:
             feeds = [l.strip() for l in open('feeds.txt', 'r')]
-            collect(feeds)
+            collect(feeds, on_article=broadcast)
             sleep(INTERVAL)
         except (KeyboardInterrupt, SystemExit):
             break
-        except:
-            dsn = os.environ.get('SENTRY_DSN', None)
-            if dsn is not None:
-                client = Client(dsn)
+        except Exception as e:
+            logger.exception(e)
+            if client is not None:
                 client.captureException()
             else:
                 raise
